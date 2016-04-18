56af7187a8ba07237a0cdc51608bd60f347f27dd
nefilskyl <cernypav.23@gmail.com>
2016-04-18 17:51:07 +0200

    Added files via upload

--- main.cpp
+++ main.cpp
@@ -1,16 +1,20 @@
-#include "ui_main.h"
+#include "ui_main.h" // hlavickovy soubor popisujici (graficke) uzivatelske rozhrani
 
 
 int main(int argc, char *argv[]){
 
-  QApplication app(argc, argv);
-  QMainWindow *window = new QMainWindow;
-  Ui::MainWindow ui;
-  ui.setupUi(window);
+  QApplication app(argc, argv); // QApplication - Qt trida, app jeji instance (=konkretni objekt), vzdy (asi) se ji pri volani predavaji argv, argc
+  QMainWindow *window = new QMainWindow; // window je ukazatel na instanci tridy QMainWindow
+  Ui::MainWindow ui; // Ui a MainWindow - viz ui_main.h, :: -> je clenem, ui je objekt tridy MainWindow
+  ui.setupUi(window); // volani metody setupUi() tridy Ui_MainWindow -> nastaveni ui, jako parametr prebira ukazatel na QMainWindow
+
+
+
+
+
 
   
-  window->show();
+  window->show(); // zobrazeni okna
   
-  return app.exec();
-
+  return app.exec(); // nekonecna smycka zajistujici interakci.. program <-> ui <-> uzivatel
 }
